b093c50db8e230930dca7b465aca0f1ef3108406
Tamara <tamararikic11@gmail.com>
2022-07-15 20:13:10 +0200

    Nested resources - dodato

---
+++ pesma-zadatak/app/Http/Controllers/AlbumPesmaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pesma;
+use Illuminate\Http\Request;
+
+class AlbumPesmaController extends Controller
+{
+  
+    public function index($album_id)
+    {
+        $pesmas = Pesma::get()->where('album_id', $album_id);
+        if (is_null($pesmas))
+            return response()->json('Data not found', 404);
+        return response()->json($pesmas);
+    }
+}
--- pesma-zadatak/routes/api.php
+++ pesma-zadatak/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\AlbumPesmaController;
 use App\Http\Controllers\IzvodjacController;
 use App\Http\Controllers\PesmaController;
 use Illuminate\Http\Request;
@@ -23,16 +24,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 
-//Route::get('/pesmas', [PesmaController::class, 'index']);
-//Route::get('/pesmas/{id}', [PesmaController::class, 'show']);
+Route::get('/pesmas', [PesmaController::class, 'index']);
+Route::get('/pesmas/{id}', [PesmaController::class, 'show']);
 
-//Route::get('/izvodjacs', [IzvodjacController::class, 'index']);
-//Route::get('/izvodjacs/{id}', [IzvodjacController::class, 'show']);
+Route::get('/izvodjacs', [IzvodjacController::class, 'index']);
+Route::get('/izvodjacs/{id}', [IzvodjacController::class, 'show']);
 
+
+Route::get('/albums', [AlbumController::class, 'index']);
+Route::get('/albums/{id}', [AlbumController::class, 'show']);
 
 Route::resource('pesmas', PesmaController::class);
 Route::resource('izvodjacs', IzvodjacController::class);
 Route::resource('albums', AlbumController::class);
 
-Route::get('/albums', [AlbumController::class, 'index']);
-Route::get('/albums/{id}', [AlbumController::class, 'show']);
+
+
+
+
+Route::resource('albums.pesmas', AlbumPesmaController::class)->only(['index']);
+//Route::get('/albums/{id}/pesmas', [AlbumPesmaController::class, 'index'])->name('albums.pesmas.index');
